444166d7ff90207e541026cb98a9a0dc473b54ad
Uriah08 <lorenz08.flores@gmail.com>
2024-10-19 13:59:55 +0800

    Fix Typo 2

--- src/components/forms/CreateProject.tsx
+++ src/components/forms/CreateProject.tsx
@@ -1,83 +1,70 @@
-"use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { 
-    Form, 
-    FormControl, 
-    FormField, 
-    FormItem, 
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
     FormLabel,
     FormMessage
-} from "../ui/form"
+} from "../ui/form";
 
-import { Input } from "../ui/input"
-import { Textarea } from "../ui/textarea"
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
+import { Calendar } from "../ui/calendar";
 
-import { CalendarIcon } from "@radix-ui/react-icons"
-import { format } from "date-fns"
-import { Calendar } from "../ui/calendar"
+import { Button } from "../ui/button";
 
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import { Button } from "../ui/button"
+import { projectSchema } from "@/src/schemas";
+import { useSession } from "next-auth/react";
+import { useCreateProjectMutation } from "@/src/store/api";
+import { useToast } from "@/src/hooks/use-toast";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "../ui/dialog";
 
-import { projectSchema } from "@/src/schemas"
-
-import { useSession } from "next-auth/react"
-
-import { formatISO } from 'date-fns'
-import { useCreateProjectMutation } from "@/src/store/api"
-
-import { useToast } from "@/src/hooks/use-toast"
-
-const CreateProject = () => {
-
-    const { data: session } = useSession()
-
-    const { toast } = useToast()
-
-    const [createProject, { isLoading }] = useCreateProjectMutation()
+const CreateProject: React.FC = () => {
+    const { data: session } = useSession();
+    const { toast } = useToast();
+    const [createProject, { isLoading }] = useCreateProjectMutation();
 
     const form = useForm<z.infer<typeof projectSchema>>({
         resolver: zodResolver(projectSchema),
         defaultValues: {
             name: "",
             description: "",
-            userId: "",
+            userId: session?.user?.id
         },
     });
 
     const onSubmit = async (values: z.infer<typeof projectSchema>) => {
-
-        const projectData = {
-            ...values,
-            userId: session?.user?.id,
-            startDate: formatISO(values.startDate), // Format as ISO string
-            endDate: formatISO(values.endDate),
-        };
-
-            try {
-                await createProject(projectData).unwrap()
-                toast({
-                    title: "Project Created",
-                    description: "Your project has been created successfully!",
-                })
-                form.reset();
-            } catch (error) {
-                console.log(error);
-                toast({
-                    title: "Error Creating Project",
-                    description: "An error occurred while creating your project. Please try again.",
-                })
-            }
-    }
-    
-    
+      const projectData = {
+          ...values,
+          userId: session?.user?.id,
+          startDate: new Date(values.startDate),
+          endDate: new Date(values.endDate),
+      };
+  
+      try {
+          await createProject(projectData).unwrap();
+          toast({
+              title: "Project Created",
+              description: "Your project has been created successfully!",
+          });
+          form.reset();
+      } catch (error) {
+          console.log(error);
+          toast({
+              title: "Error Creating Project",
+              description: "An error occurred while creating your project. Please try again.",
+          });
+      }
+  };
+  
 
     return (
         <Form {...form}>
@@ -113,30 +100,29 @@ const CreateProject = () => {
                         render={({ field }) => (
                             <FormItem className="flex flex-col">
                                 <FormLabel>Start Date</FormLabel>
-                                <Popover>
-                                    <PopoverTrigger asChild>
+                                <Dialog>
+                                    <DialogTrigger asChild>
                                         <FormControl>
                                             <Button
                                                 variant={'outline'}
-                                                className={`w-[220px] ${!field.value && "text-muted-foreground"}`}>
-                                                {field.value ? (
-                                                    format(new Date(field.value), "PPP")
-                                                ) : (
-                                                    <span>Pick a date</span>
-                                                )}
+                                                className={`w-[220px] ${!field.value && "text-muted-foreground"}`}
+                                            >
+                                                {field.value ? format(new Date(field.value), "PPP") : <span>Pick a date</span>}
                                                 <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                             </Button>
                                         </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto p-0" align="start">
+                                    </DialogTrigger>
+                                    <DialogContent aria-describedby={undefined} className="w-auto p-4 dark:bg-myDark bg-myLight">
+                                      <DialogTitle>Start Date</DialogTitle>
                                         <Calendar
                                             mode="single"
                                             selected={field.value ? new Date(field.value) : undefined}
-                                            onSelect={(date) => field.onChange(date?.toISOString())}
+                                            onSelect={(date) => date && field.onChange(date.toISOString())}
                                             initialFocus
                                         />
-                                    </PopoverContent>
-                                </Popover>
+                                    </DialogContent>
+                                </Dialog>
+                                <FormMessage />
                             </FormItem>
                         )}
                     />
@@ -146,30 +132,28 @@ const CreateProject = () => {
                         render={({ field }) => (
                             <FormItem className="flex flex-col">
                                 <FormLabel>End Date</FormLabel>
-                                <Popover>
-                                    <PopoverTrigger asChild>
+                                <Dialog>
+                                    <DialogTrigger asChild>
                                         <FormControl>
                                             <Button
                                                 variant={'outline'}
-                                                className={`w-[220px] ${!field.value && "text-muted-foreground"}`}>
-                                                {field.value ? (
-                                                    format(new Date(field.value), "PPP")
-                                                ) : (
-                                                    <span>Pick a date</span>
-                                                )}
+                                                className={`w-[220px] ${!field.value && "text-muted-foreground"}`}
+                                            >
+                                                {field.value ? format(new Date(field.value), "PPP") : <span>Pick a date</span>}
                                                 <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                             </Button>
                                         </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto p-0" align="start">
+                                    </DialogTrigger>
+                                    <DialogContent aria-describedby={undefined} className="w-auto p-4 dark:bg-myDark bg-myLight">
+                                    <DialogTitle>End Date</DialogTitle>
                                         <Calendar
                                             mode="single"
                                             selected={field.value ? new Date(field.value) : undefined}
-                                            onSelect={(date) => field.onChange(date?.toISOString())}
+                                            onSelect={(date) => date && field.onChange(date.toISOString())}
                                             initialFocus
                                         />
-                                    </PopoverContent>
-                                </Popover>
+                                    </DialogContent>
+                                </Dialog>
                                 <FormMessage />
                             </FormItem>
                         )}
@@ -181,6 +165,6 @@ const CreateProject = () => {
             </form>
         </Form>
     );
-}
+};
 
-export default CreateProject
+export default CreateProject;
